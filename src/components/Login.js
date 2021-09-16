f89cd2ed629380178e127227b5aef38f1366b329
Nazmuz Pranto <npranto@gmail.com>
2021-09-16 17:00:21 -0400

    setting up firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,21 +3,81 @@ import render from "../utils/render";
 const Login = () => {
   return `
     <section class="Login">
-      <h1 class="text-center">Login</h1>
-      <div class="p-3 text-center">
-        <a class="btn btn-primary" href="?" role="button">Home</a>
-        <a class="btn btn-success" href="?page=signup" role="button">Sign Up</a>
-      </div>
+      <h1 class="text-center mt-5 mb-5 text-muted"> Log Trade </h1>
+      <form class="form-signin" id="signin-form" action="#">       
+        <h2 class="form-signin-heading">Login</h2>
+        <input 
+          type="email" 
+          class="form-control" 
+          name="signin-email" 
+          placeholder="Email Address" 
+          autofocus 
+          required 
+        />
+        <input 
+          type="password" 
+          class="form-control" 
+          name="signin-password" 
+          placeholder="Password" 
+          required 
+        />      
+        <label class="checkbox mt-3">
+          <input 
+            type="checkbox" 
+            value="remember-me" 
+            class="remember-me" 
+            id="rememberMe" 
+            name="rememberMe"
+          /> Remember me
+        </label>
+        <div class="d-grid gap-2">
+          <button 
+            class="btn btn-lg btn-primary btn-block" 
+            type="submit"
+          >
+            Login
+          </button>   
+        </div>
+        <div class="d-grid gap-2 mt-3">
+          <a class="btn btn-link" href="?page=signup" role="button">Create new account</a>
+        </div>
+      </form>
     </section>
   `
 };
 
 const styles = () => `
   .Login {
-    padding: 3em 0;
+    padding-top: 80px;
+    padding-bottom: 80px;
     height: 100vh;
-    display: flex;
-    flex-direction: column;
+    box-sizing: border-box;
+  }
+  .form-signin {
+    max-width: 380px;
+    padding: 15px 35px 45px;
+    margin: 0 auto;
+    background-color: #fff;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+  }
+  .form-signin .form-signin-heading,
+  .form-signin .checkbox {
+    margin-bottom: 30px;
+  }
+  .form-signin .checkbox {
+    font-weight: normal;
+  }
+  .form-signin .form-control {
+    position: relative;
+    font-size: 16px;
+    height: auto;
+    padding: 10px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  .form-signin .form-control:focus {
+    z-index: 2;
   }
 `;
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,21 +3,86 @@ import render from "../utils/render";
 const Signup = () => {
   return `
     <section class="Signup">
-      <h1 class="text-center">Sign Up</h1>
-      <div class="p-3 text-center">
-        <a class="btn btn-primary" href="?" role="button">Home</a>
-        <a class="btn btn-success" href="?page=login" role="button">Login</a>
-      </div>
+      <h1 class="text-center mt-5 mb-5 text-muted"> Log Trade </h1>
+      <form class="form-signup" id="signup-form" action="#">       
+        <h2 class="form-signup-heading">Sign Up</h2>
+        <input 
+          type="text" 
+          class="form-control" 
+          name="signup-fullname" 
+          placeholder="Full Name" 
+          required
+          autofocus 
+        />
+        <input 
+          type="email" 
+          class="form-control" 
+          name="signup-email" 
+          placeholder="Email Address" 
+          required
+        />
+        <input 
+          type="password" 
+          class="form-control" 
+          name="signup-password" 
+          placeholder="Password" 
+          required
+        />    
+        <input 
+          type="password" 
+          class="form-control" 
+          name="signup-confirm-password" 
+          placeholder="Confirm Password" 
+          required
+        />    
+        <div class="d-grid gap-2 mt-3">
+          <button 
+            class="btn btn-lg btn-primary btn-block" 
+            type="submit"
+          >
+            Sign Up
+          </button>   
+        </div>
+        <div class="d-grid gap-2 mt-3">
+          <a class="btn btn-link" href="?page=login" role="button">Already have account?</a>
+        </div>
+      </form>
     </section>
   `
 };
 
 const styles = () => `
   .Signup {
-    padding: 3em 0;
+    padding-top: 80px;
+    padding-bottom: 80px;
+    box-sizing: border-box;
     height: 100vh;
-    display: flex;
-    flex-direction: column;
+  }
+  .form-signup {
+    max-width: 380px;
+    padding: 15px 35px 45px;
+    margin: 0 auto;
+    background-color: #fff;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+  }
+  .form-signup .form-signup-heading,
+  .form-signup .checkbox {
+    margin-bottom: 30px;
+  }
+  .form-signup .checkbox {
+    font-weight: normal;
+  }
+  .form-signup .form-control {
+    position: relative;
+    font-size: 16px;
+    height: auto;
+    padding: 10px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  .form-signup .form-control:focus {
+    z-index: 2;
   }
 `;
 
