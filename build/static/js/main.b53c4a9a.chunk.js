bfd76fcd52595511a4f188ec169c1d1567b70dd7
Nazmuz Pranto <npranto@gmail.com>
2021-10-10 13:12:30 -0400

    working on dashboard component

--- build/static/js/main.b53c4a9a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonplogtrade=this.webpackJsonplogtrade||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);n(1);var c=n(3),o=n.n(c),r=(n(8),n(9),n(0));var s=function(){return Object(r.jsx)("div",{className:"App",children:Object(r.jsx)("h1",{children:"Welcome to LogTrade!"})})};o.a.render(Object(r.jsx)(s,{}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.b53c4a9a.chunk.js.map
--- src/App.js
+++ src/App.js
@@ -1,13 +1,71 @@
+import React, { Component } from 'react';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import './App.css';
 
-function App() {
+const MonthlyCalendar = props => {
   return (
-    <div className="App">
-      <h1>
-        Welcome to LogTrade!
-      </h1>
+    <section className="MonthlyCalendar">
+      <header className="flex py-2 px-2 justify-between flex-wrap">
+        <MonthNavigator className="pr-2" />
+        <h1 className="active-date sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 pr-2 text-center">November 1, 2021</h1>
+        <div className="monthly-stats flex pr-2">
+          <p className="gains pr-2">
+            <sub className="text-gray-900">Gains: </sub> 
+            <span className="text-green-900 title-font sm:text-4xl text-3xl font-medium"> $254 </span>
+          </p>
+          <p className="losses text-red-900 pr-2">
+            <sub className="text-gray-900">Losses: </sub> 
+            <span className="text-red-900 title-font sm:text-4xl text-3xl font-medium"> $100 </span>
+          </p>
+          <p className="p-l text-gray-900 pr-2">
+            <sub className="text-gray-900">P/L: </sub> 
+            <span className="text-gray-900 title-font sm:text-4xl text-3xl font-medium"> $154 </span> 
+          </p>
+        </div>
+      </header>
+    </section>
+  )
+};
+
+const MonthNavigator = (props = {}) => {
+  const { 
+    prevMonth = 'October', 
+    nextMonth = 'December', 
+  } = props;
+  
+  // if (!prevMonth || !nextMonth) return ``;
+
+  return (
+    <div class="MonthNavigator flex items-center">
+      <button type="button" class="inline-flex items-center justify-center px-2 py-1 border border-transparent text-base font-small text-white bg-indigo-600 hover:bg-indigo-700">
+        <AiOutlineArrowLeft className="mr-1" /> {prevMonth}
+      </button>
+
+      <button type="button" class="inline-flex items-center justify-center px-2 py-1 ml-1 border border-transparent text-base font-small text-white bg-indigo-600 hover:bg-indigo-700">
+        {nextMonth} <AiOutlineArrowRight className="ml-1" />
+      </button>
     </div>
   );
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todayDate: new Date(),
+      activeDate: new Date(),
+      tradeLogs: [],
+    }
+  }
+
+  render() {
+    return (
+      <div className="App Dashboard">
+        <MonthlyCalendar {...this.props} {...this.state} />
+      </div>
+    ); 
+  }
 }
 
 export default App;
