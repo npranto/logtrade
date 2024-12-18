a8da4a94ae020c7362717313d7086ddffef65449
Nazmuz Pranto <npranto@gmail.com>
2024-12-18 11:31:23 -0500

    #93 - testing auto deploy

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
+        Test 1
         <ul className="bg-green-200 p-4 mb-4 flex space-x-2">
           <li>
             <Link href={'/'}>Home</Link>
