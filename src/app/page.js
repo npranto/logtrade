637e5bff37303f4b65bbdad646166c9bfc6d951c
Nazmuz Pranto <npranto@gmail.com>
2024-12-18 11:17:37 -0500

    #93 - updates layout content for each route and proper spacing

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen px-4">
       <main className="flex flex-col gap-8 flex-grow">Home Page</main>
     </div>
   );
