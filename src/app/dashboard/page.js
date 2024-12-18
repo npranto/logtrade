411f531cf03422cae16c95c5b81f5790a72c402e
Nazmuz Pranto <npranto@gmail.com>
2024-12-18 11:09:09 -0500

    #93 - [v2] - Setup routes for home, login, signup, dashboard and account pages

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="min-h-[415px] p-8 rounded-lg">Dashboard Page</div>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ src/app/signup/page.js
@@ -0,0 +1,9 @@
+const SignupPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="min-h-[415px] p-8 rounded-lg">Signup Page</div>
+    </div>
+  );
+};
+
+export default SignupPage;
